f2d5f4d15fcb27d3d4a492ef1b11f3e9bc40db7d
dengheng12 <1941106766@qq.com>
2020-11-07 23:02:42 +0800

    完善音乐播放图标

--- music/js/index.js
+++ music/js/index.js
@@ -4,18 +4,28 @@ $(function() {
     });
 
     console.log($(".content_list"))
+    //鼠标移入事件
     $(".music_con").on("mouseenter",'.list_music',function() {
         $(this).find(".list_menu").stop().fadeIn(100)
         $(this).find(".list_time a").stop().fadeIn(100)
         $(this).find(".list_time span").stop().fadeOut(100)
     })
+    //鼠标移出事件
     $(".music_con").on("mouseleave",'.list_music',function() {
         $(this).find(".list_time span").stop().fadeIn(100)
         $(this).find(".list_menu").stop().fadeOut(100)
         $(this).find(".list_time a").stop().fadeOut(100)
     })
+    //选中事件
     $(".music_con").on("click",".list_check",function() {
         $(this).toggleClass("list_checked")
+    })
+    //播放事件
+    $(".music_con").on("click",".list_menu_play",function() {
+        //当前播放
+       $(this).toggleClass("list_menu_play2");
+       //复原其他播放
+       $(this).parent(".list_music")
     })
 
     getMusic()
@@ -44,7 +54,7 @@ $(function() {
        <div class="list_number">${index+1}</div>
        <div class="list_name">${item.name}
            <div class="list_menu">
-               <a href="javascript:;" title="播放"></a>
+               <a href="javascript:;" class="list_menu_play" title="播放"></a>
                <a href="javascript:;" title="添加"></a>
                <a href="javascript:;" title="下载"></a>
                <a href="javascript:;" title="分享"></a>
